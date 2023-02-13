abd16ad1aaac3cf440b3acf81dc93e697c761361
Mehrajul Islam~ <codermehraj@gmail.com>
2023-02-13 09:51:45 +0600

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vaara_app/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: WelcomeScreen(),
     );
   }
 }
